3a95093a9be47c928d266d9b70eb7e31ca5abd4c
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-07-05 20:03:10 +0900

    스프링 시큐리티

--- src/main/java/com/nuey/spring_security/config/SecurityConfig.java
+++ src/main/java/com/nuey/spring_security/config/SecurityConfig.java
@@ -20,6 +20,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+		// jwt방식은 상태 stateless로 관리하기 때문에 csrf를 방어하지 않아도 되서 disabled
+		http
+				.csrf(auth -> auth.disable());
+
+		// form 로그인 방식 disable
+		http
+				.formLogin(auth -> auth.disable());
+
+		// http basic 인증 방식 disable
+		http
+				.httpBasic(auth -> auth.disable());
+
+		// 경로별 인가 작업
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/signup").permitAll()
